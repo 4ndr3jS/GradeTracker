2e8b07d822548d7af71086e7c401d978ce8dde2e
4ndr3j <andrejstanic3@gmail.com>
2025-12-15 16:36:56 +0100

    Added a function to add courses

---
+++ index.js
@@ -0,0 +1,22 @@
+let courses = [];
+
+function addCourse(){
+    const name = document.getElementById('courseName').ariaValueMax.trim();
+    const grade = document.getElementById('grade').value;
+
+    if(!name)
+        alert("Please enter a name for the subject/course");
+    courses.push({
+        id: Date.now(),
+        name: name,
+        grade: grade
+    });
+    renderCourses();
+    calculateGPA();
+}
+
+function deleteCourse(id){
+    courses = courses.filter(course => course.id !==id);
+    renderCourses();
+    calculateGPA();
+}
